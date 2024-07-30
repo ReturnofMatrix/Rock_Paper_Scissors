64eddc21c500d95f7a3de8b1faaeabfe1a64c19a
ReturnofMatrix <roshanbhogan007@gmail.com>
2024-07-30 11:28:00 +0530

    Coded the getComputerChoice and prompt function with the help of Math.random and prompt.

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
-console.log("Lets")
+let random_no,comp_choice;
+function getComputerChoice(){
+	random_no=Math.random()*100+1;
+	random_no=random_no.toFixed(2);
+}
+getComputerChoice();
+
+if(random_no<=33.33){
+comp_choice="Rock";
+}else if(random_no<=66.66){
+comp_choice="Paper";
+}else{
+comp_choice="Scissors";
+}
+
+const player_choice=prompt("Choose between Rock , Paper or Scissors : ");
+console.log(player_choice,comp_choice);
+
